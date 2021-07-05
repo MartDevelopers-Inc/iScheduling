b6b0489f23dd23bc01157cf824389f2cca15b71c
MartMbithi <martdevelopers254@gmail.com>
2021-07-05 10:52:48 +0300

    Crafted Search Results For Staffs

--- views/staff_search_result.php
+++ views/staff_search_result.php
@@ -23,28 +23,6 @@ session_start();
 require_once('../config/config.php');
 require_once('../config/checklogin.php');
 check_login();
-
-/* Delete Staff */
-if (isset($_GET['delete'])) {
-    $delete = $_GET['delete'];
-    $login = $_GET['login'];
-    $adn = "DELETE FROM Clinic_Staff WHERE Staff_id=?";
-    $delete_auth = "DELETE FROM Login WHERE Login_id = ?";
-    $stmt = $mysqli->prepare($adn);
-    $auth_stmt = $mysqli->prepare($delete_auth);
-    $stmt->bind_param('s', $delete);
-    $auth_stmt->bind_param('s', $login);
-    $stmt->execute();
-    $auth_stmt->execute();
-    $stmt->close();
-    $auth_stmt->close();
-    if ($stmt && $auth_stmt) {
-        $success = "Deleted" && header("refresh:1; url=staffs");
-    } else {
-        $info = "Please Try Again Or Try Later";
-    }
-}
-
 require_once('../partials/head.php');
 ?>
 
@@ -99,50 +77,6 @@ require_once('../partials/head.php');
     <!-- Side Nav Wrapper-->
     <?php require_once('../partials/side_nav.php'); ?>
 
-    <!-- Add new Staff modal-->
-    <div class="add-new-contact-modal modal fade px-0" id="addnewcontact" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addnewcontactlabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <div class="d-flex align-items-center justify-content-between mb-4">
-                        <h6 class="modal-title" id="addnewcontactlabel">New Staff</h6>
-                        <button class="btn btn-close p-1 ms-auto me-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <form method="POST">
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="fullname">Full Name</label>
-                            <input class="form-control" required name="Staff_full_name" type="text">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="email">Email Address</label>
-                            <input class="form-control" required name="Staff_email" type="email">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="job">ID Number</label>
-                            <input class="form-control" required type="text" name="Staff_id_no">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="job">Phone Number</label>
-                            <input class="form-control" required name="Staff_phone_no" type="text">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="Username">Login Username</label>
-                            <input class="form-control" required name="Login_user_name">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="Username">Login Email</label>
-                            <input class="form-control" required name="Login_email" type="email">
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="form-label" for="fullname">Login Password</label>
-                            <input class="form-control" required name="Login_password" type="password">
-                        </div>
-                        <button class="btn btn-success w-100" name="AddStaff" type="submit">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="page-content-wrapper py-3">
         <!-- Add New Staff-->
         <div class="add-new-contact-wrap"><a class="shadow" href="#" data-bs-toggle="modal" data-bs-target="#addnewcontact"><i class="bi bi-plus"></i></a></div>
@@ -152,7 +86,7 @@ require_once('../partials/head.php');
                     <div class="chat-search-box">
                         <form action="staff_search_result" method="GET">
                             <div class="input-group"><span class="input-group-text" id="searchbox"><i class="bi bi-search"></i></span>
-                                <input class="form-control" name="querry" type="text" placeholder="Search Staffs" aria-describedby="searchbox">
+                                <input class="form-control" type="search" placeholder="Search Staffs" aria-describedby="searchbox">
                             </div>
                         </form>
                     </div>
@@ -160,39 +94,48 @@ require_once('../partials/head.php');
             </div>
             <!-- Element Heading-->
             <div class="element-heading">
-                <h6 class="ps-1">Clinic Staffs</h6>
+                <h6 class="ps-1">Clinic Staffs Search Results </h6>
             </div>
             <!-- Chat User List-->
             <ul class="ps-0 chat-user-list">
                 <?php
-                $ret = "SELECT * FROM `Clinic_Staff`";
-                $stmt = $mysqli->prepare($ret);
-                $stmt->execute(); //ok
-                $res = $stmt->get_result();
-                while ($staff = $res->fetch_object()) {
+                $search_query = htmlspecialchars($_GET['search_query']);
+                $min_length = 0;
+                if (strlen($search_query) >= $min_length) {
+                    $search_query = mysqli_real_escape_string($mysqli, $search_query);
+                    $raw_results = mysqli_query($mysqli, "SELECT * FROM Clinic_Staff WHERE (`Staff_full_name` LIKE '%" . $search_query . "%')  ");
+                    if (mysqli_num_rows($raw_results) > 0) {
+                        while ($results = mysqli_fetch_array($raw_results)) {
                 ?>
-                    <li class="p-3 chat-unread"><a class="d-flex" href="staff?view=<?php echo $staff->Staff_id; ?>">
-                            <!-- Thumbnail-->
-                            <div class="chat-user-thumbnail me-3 shadow"><img class="img-circle" src="../public/img/bg-img/profile.svg" alt=""><span class="active-status"></span></div>
-                            <!-- Info-->
-                            <div class="chat-user-info">
-                                <h6 class="text-truncate mb-0"><?php echo $staff->Staff_full_name; ?></h6>
-                                <h6 class="text-truncate mb-0">Email: <?php echo $staff->Staff_email; ?></h6>
-                                <h6 class="text-truncate mb-0">Phone: <?php echo $staff->Staff_phone_no; ?></h6>
-                                <h6 class="text-truncate mb-0">ID NO: <?php echo $staff->Staff_id_no; ?></h6>
-
-                            </div>
-                        </a>
-                        <!-- Options-->
-                        <div class="dropstart chat-options-btn">
-                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-three-dots-vertical"></i></button>
-                            <ul class="dropdown-menu">
-                                <li><a href="staffs?delete=<?php echo $staff->Staff_id; ?>"><i class="bi bi-trash"></i>Remove</a></li>
-                            </ul>
-                        </div>
-                    </li>
+                            <li class="p-3 chat-unread"><a class="d-flex" href="staff?view=<?php echo $results['Staff_id']; ?>">
+                                    <!-- Thumbnail-->
+                                    <div class="chat-user-thumbnail me-3 shadow"><img class="img-circle" src="../public/img/bg-img/profile.svg" alt=""><span class="active-status"></span></div>
+                                    <!-- Info-->
+                                    <div class="chat-user-info">
+                                        <h6 class="text-truncate mb-0"><?php echo $results['Staff_full_name']; ?></h6>
+                                        <h6 class="text-truncate mb-0">Email: <?php echo $results['Staff_email']; ?></h6>
+                                        <h6 class="text-truncate mb-0">Phone: <?php echo $results['Staff_phone_no']; ?></h6>
+                                        <h6 class="text-truncate mb-0">ID NO: <?php echo $results['Staff_id_no']; ?></h6>
+                                    </div>
+                                </a>
+                                <!-- Options-->
+                                <div class="dropstart chat-options-btn">
+                                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-three-dots-vertical"></i></button>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="staffs?delete=<?php echo $results['Staff_id']; ?>"><i class="bi bi-trash"></i>Remove</a></li>
+                                    </ul>
+                                </div>
+                            </li>
                 <?php
-                } ?>
+                        }
+                    } else {
+                        echo "<h5  class='text-danger text-center'> No Search Results</h5>";
+                    }
+                } else {
+                    echo "<h5  class='text-danger text-center'>Minimum Search Querry  Length Is " . $min_length . " Characters</h5>";
+                }
+
+                ?>
 
             </ul>
         </div>
